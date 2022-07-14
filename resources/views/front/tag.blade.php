3c60ace5e19aa8532a063af025efec57290ff92e
Sigit Wahyudi <wahyudi.sigit@gmail.com>
2022-07-14 09:58:11 +0800

    tag and category list

--- resources/views/front/tag.blade.php
+++ resources/views/front/tag.blade.php
@@ -8,22 +8,14 @@
         <div class="row">
 
                 @foreach ($items as $item)
-                {{-- <a href="/{{$item->url_seo}}"><h5>{{ ucwords ( $item->title ) }}</h5></a> --}}
                 <div class="col-md-4">
                     <div class="card">
                         <div class="card-body text-secondary">
                             <a href="/{{$item->url_seo}}"><h5 class="card-title">{{ ucwords ( $item->title ) }}</h5></a>
                             <p class="card-text">
-                                @if($item->modified) 
-                                <p class="card-text">
-                                    <small class="">terakhir diedit : {{ date('d F Y, H:i', strtotime($item->modified)) }}</small>
-                                </p>
-                                    
-                                @else
                                 <p class="card-text">
                                     <small class="">terbit : {{ date('d F Y, H:i', strtotime($item->created)) }}</small>
                                 </p>
-                                @endif
                             </p>
                         </div>
                     </div> <!-- end card-box-->
